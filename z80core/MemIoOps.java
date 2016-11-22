9477f33869818b467663da7030385f0893c19b29
Douglas Miller <durgadas311@gmail.com>
2016-11-22 09:45:00 -0600

    Remove CRs and convert indentation to tabs.

--- z80core/MemIoOps.java
+++ z80core/MemIoOps.java
@@ -10,15 +10,15 @@ package z80core;
  * @author jsanchez
  */
 public interface MemIoOps {
-    int fetchOpcode(int address);
+	int fetchOpcode(int address);
 
-    int peek8(int address);
-    void poke8(int address, int value);
-    int peek16(int address);
-    void poke16(int address, int word);
+	int peek8(int address);
+	void poke8(int address, int value);
+	int peek16(int address);
+	void poke16(int address, int word);
 
-    int inPort(int port);
-    void outPort(int port, int value);
+	int inPort(int port);
+	void outPort(int port, int value);
 
-    void contendedStates(int address, int tstates);
+	void contendedStates(int address, int tstates);
 }
